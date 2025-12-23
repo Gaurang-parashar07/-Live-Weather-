585145486c9bf1e1217241918d635e67ffe89538
Gaurang Parashar <parashargaurang07@gamil.com>
2025-12-24 01:42:35 +0530

    remove comment

--- weather.js
+++ weather.js
@@ -1,33 +1,4 @@
-// const apikey = "a61acc0ea485e854e1bb01b4adff661b";
-// const apiurl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=&lon=";
-
-// const searchbox = document.querySelector(".search input");
-// const searchbtn = document.querySelector(".search svg")
-
-// async function checkweather(city){
-//     const response = await fetch(`${apiurl}${city}&appid=${apikey}` );
-
-//     var data = await response.json();
-
-//     console.log(data); 
-
-//     document.querySelector(".city").innerHTML = data.name;
-//     document.querySelector(".temp").innerHTML = Math.round(data.main.temp )+ "°C ";
-//     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-//     document.querySelector(".Wind").innerHTML = data.wind.speed + "km/h";
-// }
-// searchbtn.addEventListener("click",()=>{
-//     checkweather(searchbox.value);  
-// })
-
-// searchbox.addEventListener("keypress", (e) => {
-//     if (e.key === "Enter") {
-//         checkweather(searchbox.value);
-//     }
-// });
-
-
-const apikey = "a61acc0ea485e854e1bb01b4adff661b";
+ const apikey = "a61acc0ea485e854e1bb01b4adff661b";
 
 async function getCoordinates(city) {
     const geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apikey}`;
